77647deb758d8ac7d4b72c071c51ca97dbe037fc
legokna <leo_dan2010@hotmail.com>
2020-05-24 16:16:43 -0500

    correcion en el archivo de datos de la BD

--- springData.sql
+++ springData.sql
@@ -155,11 +155,11 @@ UNLOCK TABLES;
 -- Dumping data for table `hibernate_sequence`
 --
 
-LOCK TABLES `hibernate_sequence` WRITE;
+/*LOCK TABLES `hibernate_sequence` WRITE;*/
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (40);
+/*INSERT INTO `hibernate_sequence` VALUES (40);*/
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
-UNLOCK TABLES;
+/*UNLOCK TABLES;*/
 
 --
 -- Dumping data for table `medicamentos`
