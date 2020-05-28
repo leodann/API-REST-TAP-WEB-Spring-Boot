2d998c23535564183254a9a00ed114c9893036fa
legokna <leo_dan2010@hotmail.com>
2020-05-28 10:08:07 -0500

    configuracion del logg

--- src/main/java/com/tapweb/apiresttapweb/ApiRestTapWebApplication.java
+++ src/main/java/com/tapweb/apiresttapweb/ApiRestTapWebApplication.java
@@ -1,13 +1,24 @@
 package com.tapweb.apiresttapweb;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-public class ApiRestTapWebApplication {
-
+@Slf4j
+public class ApiRestTapWebApplication {    
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(ApiRestTapWebApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(ApiRestTapWebApplication.class, args);
+    }
+
+    @EventListener  
+    private void onAplicatioStarted(ApplicationStartedEvent applicationStartedEvent){
+        log.info("Apilcation Started");   
     }
 
 }
